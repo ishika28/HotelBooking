893451c1691ea95c01af8716edc3385d64861cec
ishika28 <isu.pradhan@gmail.com>
2019-11-21 04:59:43 +0545

    update

--- app/src/main/java/com/example/hotelbooking/MainActivity.java
+++ app/src/main/java/com/example/hotelbooking/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,6 +14,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -21,54 +23,57 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private Spinner spinLocation;
     private Spinner spinRoomType;
-    private TextView tvCheckInDate;
-    private TextView tvCheckOutDate;
     private EditText etAdults, etChildren, etRoom;
     private Button btnCalculate;
-
-    TextView tvLocation, tvRoomType, tvInDate, tvOutDate, tvAdults, tvChildren, tvRoom, tvSErvice, tvTax, tvTotal;
-
+    TextView datepickerCheckInDate,  datepickerCheckOutDate;
+   
+    TextView tvDays, tvError, tvLocation, tvRoomType, tvInDate, tvOutDate, tvAdults, tvChildren, tvRoom, tvSErvice, tvTax, tvTotal;
+    int Year2, Year3;
+    int Month2, Month3;
+    int Day2, Day3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvCheckInDate = findViewById(R.id.tvCheckInDate);
-        tvCheckOutDate =findViewById(R.id.tvCheckOutDate);
+        datepickerCheckInDate = findViewById(R.id.datepickerCheckInDate);
+        datepickerCheckOutDate = findViewById(R.id.datepickerCheckOutDate);
         etAdults = findViewById(R.id.etAdults);
-        etChildren =  findViewById(R.id.etChildren);
+        etChildren = findViewById(R.id.etChildren);
         etRoom = findViewById(R.id.etRoom);
-        btnCalculate =findViewById(R.id.btnCalculate);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        tvError = (TextView) findViewById(R.id.tverror);
 
         //scroll view outputs data binding
-        tvLocation =  findViewById(R.id.tvLocation);
+        tvDays = findViewById(R.id.tvDays);
+        tvLocation = findViewById(R.id.tvLocation);
         tvRoomType = findViewById(R.id.tvRoomType);
-        tvInDate =  findViewById(R.id.tvInDate);
-        tvOutDate =  findViewById(R.id.tvOutDate);
-        tvAdults =  findViewById(R.id.tvAdults);
+        tvInDate = findViewById(R.id.tvInDate);
+        tvOutDate = findViewById(R.id.tvOutDate);
+        tvAdults = findViewById(R.id.tvAdults);
         tvChildren = findViewById(R.id.tvChildren);
-        tvRoom =  findViewById(R.id.tvRoom);
+        tvRoom = findViewById(R.id.tvRoom);
         tvSErvice = findViewById(R.id.tvService);
-        tvTax =  findViewById(R.id.tvTax);
-        tvTotal =  findViewById(R.id.tvTotal);
+        tvTax = findViewById(R.id.tvTax);
+        tvTotal = findViewById(R.id.tvTotal);
 
         final SimpleDateFormat formatdate = new SimpleDateFormat("yyyy/MM/dd", Locale.US);
 
         //spinner for location
-        spinLocation =findViewById(R.id.spinLocation);
-        String countries[] ={"Kathmandu","Patan","Bhaktapur"};
-        ArrayAdapter adapter =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,countries);
+        spinLocation = findViewById(R.id.spinLocation);
+        String countries[] = {"Kathmandu", "Patan", "Bhaktapur"};
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
         spinLocation.setAdapter(adapter);
 
         //spinner for room type
-        spinRoomType=findViewById(R.id.spinRoomType);
-        String roomType[] ={"AC","Delux","Normal"};
-        ArrayAdapter adapters =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,countries);
+        spinRoomType = findViewById(R.id.spinRoomType);
+        String roomType[] = {"AC", "Delux", "Normal"};
+        ArrayAdapter adapters = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, roomType);
         spinRoomType.setAdapter(adapters);
 
         //setting OnClick Listener on check in date
-        tvCheckInDate.setOnClickListener(new View.OnClickListener() {
+        datepickerCheckInDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadcheckIndate();
@@ -76,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //setting Onclick listener on check out date
-        tvCheckOutDate.setOnClickListener(new View.OnClickListener() {
+        datepickerCheckOutDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadchechkOutdate();
@@ -90,28 +95,102 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                tvLocation.setText("Location : "+spinLocation.getSelectedItem().toString());
-                tvRoomType.setText("Room Type : "+spinRoomType.getSelectedItem().toString());
-                tvInDate.setText(tvCheckInDate.getText().toString());
-                tvOutDate.setText(tvCheckOutDate.getText().toString());
-                tvAdults.setText("Number of Adults : "+etAdults.getText().toString());
-                tvChildren.setText("NUmber of Children : "+etChildren.getText().toString());
-                tvRoom.setText("NUmber of Rooms : "+etRoom.getText().toString());
+                if (TextUtils.isEmpty(tvInDate.getText())) {
+                    tvError.setText("Please enter check in Date ");
+                    return;
+                } else if (TextUtils.isEmpty(tvOutDate.getText())) {
+                    tvError.setText("Please enter checked out date ");
+                    return;
+                }
 
-                String dateIn = tvCheckInDate.getText().toString();
-                String dateOUt = tvCheckOutDate.getText().toString();
+
+                tvLocation.setText("Location : " + spinLocation.getSelectedItem().toString());
+                tvRoomType.setText("Room Type : " + spinRoomType.getSelectedItem().toString());
+
+                tvAdults.setText("Number of Adults : " + etAdults.getText().toString());
+                tvChildren.setText("Number of Children : " + etChildren.getText().toString());
+                tvRoom.setText("Number of Rooms : " + etRoom.getText().toString());
+
+                //Dates conversion for number of days
+                Calendar cal1 = Calendar.getInstance();
+                Calendar cal2 = Calendar.getInstance();
+                cal1.set(Year2, Month2, Day2);
+                cal2.set(Year3, Month3, Day3);
+                long millis1 = cal1.getTimeInMillis();
+                long millis2 = cal2.getTimeInMillis();
+                long diff = millis2 - millis1;
+                long diffDays = (diff / (86400000));
+
+                //Calculation part
+                int numRoom = Integer.parseInt(etRoom.getText().toString());
+                double price;
+                double TotalPrice;
+                double ServiceCharge;
+                double GrandTotal;
+
+
+                String roomtype = spinRoomType.getSelectedItem().toString();
+
+
+                if (roomtype == "Deluxe") {
+                    //Room Price Per Night:"+"2000
+                    price = 2000;
+                    TotalPrice = price * numRoom * diffDays;
+                    ServiceCharge = ((0.10) * TotalPrice) + TotalPrice;
+                    GrandTotal = ((0.13) * ServiceCharge) + ServiceCharge;
+
+                    tvDays.setText("Stayed Days : " + diffDays);
+                    tvRoom.setText(("Rooms booked : " + numRoom));
+                    tvTotal.setText("Total : " + TotalPrice);
+                    tvSErvice.setText("Price after service charge : " + ServiceCharge);
+                    tvTax.setText("Price after tax inclusion : " + GrandTotal);
+
+
+                    Toast.makeText(MainActivity.this, "Total cost : " + GrandTotal, Toast.LENGTH_SHORT).show();
+
+
+                } else if (roomtype == "AC") {
+
+                    price = 3000;
+                    TotalPrice = price * numRoom * diffDays;
+                    ServiceCharge = ((0.10) * TotalPrice) + TotalPrice;
+                    GrandTotal = ((0.13) * ServiceCharge) + ServiceCharge;
+
+                    tvDays.setText("Stayed Days : " + diffDays);
+                    tvRoom.setText(("Rooms booked : " + numRoom));
+                    tvTotal.setText("Total : " + TotalPrice);
+                    tvSErvice.setText("Price after service charge : " + ServiceCharge);
+                    tvTax.setText("Price after tax inclusion : " + GrandTotal);
+
+
+                    Toast.makeText(MainActivity.this, "Total cost : " + GrandTotal, Toast.LENGTH_SHORT).show();
+
+                } else if (roomtype == "Platinum") {
+                    price = 4000;
+
+                    TotalPrice = price * numRoom * diffDays;
+                    ServiceCharge = ((0.10) * TotalPrice) + TotalPrice;
+                    GrandTotal = ((0.13) * ServiceCharge) + ServiceCharge;
+
+                    tvDays.setText("Stayed Days : " + diffDays);
+                    tvRoom.setText(("Rooms booked : " + numRoom));
+                    tvTotal.setText("Total : " + TotalPrice);
+                    tvSErvice.setText("Price after service charge : " + ServiceCharge);
+                    tvTax.setText("Price after tax inclusion : " + GrandTotal);
+
+
+                    Toast.makeText(MainActivity.this, "Total cost : " + GrandTotal, Toast.LENGTH_SHORT).show();
+
+                }
 
 
             }
         });
 
-
-
     }
 
 
-    private void loadcheckIndate()
-    {
+    private void loadcheckIndate() {
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
@@ -120,15 +199,14 @@ public class MainActivity extends AppCompatActivity {
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                String date = year +"/"+month+"/"+dayOfMonth;
-                tvCheckInDate.setText(date);
+                String date = year + "/" + month + "/" + dayOfMonth;
+                tvInDate.setText(date);
             }
-        },year,month,day);
+        }, year, month, day);
         datePickerDialog.show();
     }
 
-    private void loadchechkOutdate()
-    {
+    private void loadchechkOutdate() {
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
@@ -137,10 +215,10 @@ public class MainActivity extends AppCompatActivity {
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                String date = year +"/"+month+"/"+dayOfMonth;
-                tvCheckOutDate.setText(date);
+                String date = year + "/" + month + "/" + dayOfMonth;
+                tvOutDate.setText(date);
             }
-        },year,month,day);
+        }, year, month, day);
         datePickerDialog.show();
     }
 }
